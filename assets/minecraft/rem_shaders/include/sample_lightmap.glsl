2eeddb7c3862a0ed5e61070687718937531d40b5
poisonous888 <rwlittle888@gmail.com>
2026-07-23 00:47:53 -0400

    b2

--- assets/minecraft/rem_shaders/include/sample_lightmap.glsl
+++
@@ -1,5 +0,0 @@
-#version 330
-
-vec4 sample_lightmap(sampler2D lightMap, ivec2 uv) {
-    return texelFetch(lightMap, uv / 16, 0);
-}
--- assets/minecraft/rem_shaders/include/shared_data_ids.glsl
+++
@@ -1,7 +0,0 @@
-bool shouldApplyEmissives(float sampledColorA) {
-    return int(sampledColorA * 255) == 252;
-}
-
-bool shouldApplyHalfOpaqueEmissives(float sampledColorA) {
-    return int(sampledColorA * 255) == 127;
-}
